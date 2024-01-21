11fc23ef9b4f6f1db1d981979dd3abdd8b7f123d
Documenter.jl <documenter@juliadocs.github.io>
2024-01-21 20:19:18 +0000

    build based on 4f5649f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraphVizDataFrame","category":"page"},{"location":"#GraphVizDataFrame","page":"Home","title":"GraphVizDataFrame","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraphVizDataFrame.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraphVizDataFrame]","category":"page"}]
+}
